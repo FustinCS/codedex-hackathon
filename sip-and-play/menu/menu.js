8e83f1353b070002b9eb0e394a26b09de054d1d7
Justin J <132813834+FustinCS@users.noreply.github.com>
2024-07-11 21:41:35 -0700

    Add three.js coffee cup render

--- sip-and-play/menu/menu.js
+++ sip-and-play/menu/menu.js
@@ -1,1 +1,44 @@
 import './menu.css';
+
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(50, 1, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('.webgl'),
+    alpha: true,
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(500,500);
+renderer.setClearColor(0xF4E2C1, 1);
+camera.position.set(1, 3, 2);
+
+let light = new THREE.PointLight( 0xffffff, 0.9 );
+light.intensity = 40;
+camera.add( light );
+scene.add( camera );
+
+let mesh;
+const loader = new GLTFLoader().setPath('../assets/');
+loader.load('cafe_latte_with_art.glb', (gltf) => {
+    mesh = gltf.scene;
+    scene.add(mesh);
+});
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.target.set(0, 1, 0); // Adjust the target as needed
+orbit.update();
+
+animate();
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    orbit.update();
+
+    renderer.render(scene, camera);
+}
